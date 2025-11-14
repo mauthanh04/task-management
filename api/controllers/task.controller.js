a87eef86a6d7b5f0900307d3b82cb10aad1dc759
mauthanh04 <mauthanh00004@gmail.com>
2025-11-14 10:53:00 +0700

    sap xep theo tieu chi

--- api/controllers/task.controller.js
+++ api/controllers/task.controller.js
@@ -1,5 +1,6 @@
 const Task = require('../models/task.model');
 
+// [GET] /api/tasks
 exports.index = async (req, res) => {
     const find = {
         deleted: false
@@ -9,10 +10,18 @@ exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
-    const tasks = await Task.find(find);
+    //sort sắp xếp theo yêu cầu
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey] = req.query.sortValue;
+    }
+    // end sort
+
+    const tasks = await Task.find(find).sort(sort);
     res.json(tasks);
 };
 
+// [GET] /api/tasks/detail/:id
 exports.detail = async (req, res) => {
     const id = req.params.id;
     const task = await Task.findOne({
